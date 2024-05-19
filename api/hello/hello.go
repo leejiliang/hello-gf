3a42a13fca6992abba7021997d324273ab8554e8
lee <leejiliang@126.com>
2024-05-19 12:10:01 +0800

    feat: 测试在Req中使用p标签指定参数别名, d 标签指定默认值

--- api/hello/hello.go
+++ api/hello/hello.go
@@ -9,7 +9,7 @@ import "github.com/gogf/gf/v2/frame/g"
 type ParamsReq struct {
 	//g.Meta `method: "all" `
 	g.Meta `path:"/params" method:"all" ` //路径参数
-	Username string
+	Username string `p:"name" d:"admin"` // p: 表示param: 参数别名, 请求传参可以使用的属性名, d表示默认值.
 	Password string
 	Age int
 }
