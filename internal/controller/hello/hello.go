7ecc6c7f437785ad4c1d34e56052c29a6dc6f04a
jiliang.li <leejiliang@126.com>
2024-05-20 10:35:17 +0800

    feat: 测试查询条件=,>,<,>=,<=,in,or

--- internal/controller/hello/hello.go
+++ internal/controller/hello/hello.go
@@ -79,25 +79,32 @@ func (c *Hello) Response(ctx context.Context, req *hello.ParamsReq) (res *hello.
 
 func (c *Hello) Db(req *ghttp.Request) {
 	//req.Response.Writeln(g.Model("book"))
-	//model := g.Model("book")
+	model := g.Model("book")
 	//record, err := model.One()
 	//record, err := model.Fields("id,name").All()
 	//record, err := model.Fields("id", "name").All()
 	//record, err := model.FieldsEx("id", "name").All()
 	//record, err := model.Value("name") // 表示一条数据的一个具体字段值
 	//record, err := model.Array("name") // 表示查询某一列数据的全部值
-	min, err := g.Model("book").Min("price")
-	max, err := g.Model("book").Max("price")
-	avg, err := g.Model("book").Avg("price")
-	count, err := g.Model("book").Count()
+	//min, err := g.Model("book").Min("price")
+	//max, err := g.Model("book").Max("price")
+	//avg, err := g.Model("book").Avg("price")
+	//count, err := g.Model("book").Count()
+	//all, err := model.Where("id", 3).All()
+	//all, err := model.Where("id<", 3).All()
+	//all, err := model.Where("id<?", 2).All()
+	//all, err := model.WhereLTE("id", 4).WhereGTE("id", 2).All()
+	//all, err := model.WhereIn("id", g.Array{3, 4, 5}).All()
+	all, err := model.WhereIn("id", g.Array{3, 4, 5}).WhereOr("id", 1).All()
 	if err == nil {
+		req.Response.WriteJson(all)
 		//req.Response.Writeln(record["name"])
-		req.Response.Writeln(g.Map{
-			"min":   min,
-			"max":   max,
-			"avg":   avg,
-			"count": count,
-		})
+		//req.Response.Writeln(g.Map{
+		//	"min":   min,
+		//	"max":   max,
+		//	"avg":   avg,
+		//	"count": count,
+		//})
 		//req.Response.Writeln(record[0]["name"])
 	}
 }
